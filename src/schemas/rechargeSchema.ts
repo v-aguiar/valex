ce2ea71a7ebfd496844580d3ba3e7749c5c48dc7
v-aguiar <v.aguiar.dev@gmail.com>
2022-07-10 22:48:52 -0300

    feat: Recharge a card on route POST '/recharge'

---
+++ src/schemas/rechargeSchema.ts
@@ -0,0 +1,16 @@
+﻿import Joi from "joi";
+
+const rechargeSchema = Joi.object({
+  cardId: Joi.number().required().messages({
+    "number.base": "⚠ Card ID must be a number!",
+    "number.required": "⚠ Card ID is required!",
+  }),
+
+  amount: Joi.number().min(0).required().messages({
+    "number.base": "⚠ Amount must be a number!",
+    "number.min": "⚠ Amount must be greater than 0!",
+    "number.required": "⚠ Amount is required!",
+  }),
+});
+
+export default rechargeSchema;
