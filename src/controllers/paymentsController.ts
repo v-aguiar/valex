3417da10c4ebb5f50279335ceb485e248e788fac
v-aguiar <v.aguiar.dev@gmail.com>
2022-07-11 10:25:17 -0300

    feat: Register payments from Points of Sale, on route POST '/buy'

---
+++ src/controllers/paymentsController.ts
@@ -0,0 +1,11 @@
+﻿import { Request, Response } from "express";
+
+import paymentServices from "../services/paymentServices.js";
+
+export async function buyOnPointOfSale(req: Request, res: Response) {
+  const { businessId, cardId, amount, password } = req.body;
+
+  await paymentServices.insertPayment(cardId, businessId, amount, password);
+
+  res.sendStatus(200);
+}
--- src/routes/statementsRouter.ts
+++ src/routes/statementsRouter.ts
@@ -1,13 +1,16 @@
 ﻿import { Router } from "express";
 
 import { recharge } from "../controllers/rechargeController.js";
+import { buyOnPointOfSale } from "../controllers/paymentsController.js";
 
 import apiKeyValidator from "../middlewares/apiKeyValidator.js";
 import bodySchemaValidator from "../middlewares/bodySchemaValidator.js";
 import rechargeSchema from "../schemas/rechargeSchema.js";
+import paymentSchema from "../schemas/paymentSchema.js";
 
 const statementsRouter = Router();
 
 statementsRouter.post("/recharge", apiKeyValidator, bodySchemaValidator(rechargeSchema), recharge);
+statementsRouter.post("/buy", bodySchemaValidator(paymentSchema), buyOnPointOfSale);
 
 export default statementsRouter;
