f82648f67f1b032e1f78c7b1ff89fa4e62175a22
v-aguiar <v.aguiar.dev@gmail.com>
2022-07-11 10:42:12 -0300

    fix: fix apiKeyValidator middleware

--- src/middlewares/apiKeyValidator.ts
+++ src/middlewares/apiKeyValidator.ts
@@ -1,8 +1,13 @@
 ﻿import { NextFunction, Request, Response } from "express";
 
-export default function apiKeyValidator(req: Request, res: Response, next: NextFunction) {
+import * as companyRepository from "../repositories/companyRepository.js";
+
+export default async function apiKeyValidator(req: Request, res: Response, next: NextFunction) {
   const apiKey = req.headers["x-api-key"];
-  if (apiKey !== process.env.API_KEY) {
+
+  const company = await companyRepository.findByApiKey(apiKey.toString());
+
+  if (!company) {
     throw {
       name: "unauthorized",
       message: "⚠ You don't have access to this API!",
